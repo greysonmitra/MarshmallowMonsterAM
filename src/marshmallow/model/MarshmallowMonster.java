10b75c88ba4426c622d83484673ec16a259331c3
greysonmitra <greyson.mitra@outlook.com>
2015-09-11 09:56:05 -0600

    wrote comments

--- src/marshmallow/model/MarshmallowMonster.java
+++ src/marshmallow/model/MarshmallowMonster.java
@@ -3,20 +3,23 @@ package marshmallow.model;
 public class MarshmallowMonster
 {
 	private String monsterName;
-	private int monsterEyes;
+	private int monsterEyes;                               //These are the declarations I think?
 	private int monsterNoses;
 	private double monsterHair;
 	private double monsterLegs;
 	private boolean monsterBellyButton;
 	
-	private MarshmallowMonster();
+	private MarshmallowMonster()
+	{
+		
+	}
 	
 	public MarshmallowMonster(String monsterName, int monsterEyes, int monsterNoses, double monsterHair, double monsterLegs, boolean monsterBellyButton )
-	
+	//^^^ This right here is the constructor I think?
 	{
 		this.monsterName = monsterName;
 		this.monsterEyes = monsterEyes;
-		this.monsterNoses = monsterNoses;
+		this.monsterNoses = monsterNoses;             //I think these are the methods or maybe declarations?
 		this.monsterLegs = monsterLegs;
 		this.monsterHair = monsterHair;
 		this.monsterBellyButton = monsterBellyButton;
